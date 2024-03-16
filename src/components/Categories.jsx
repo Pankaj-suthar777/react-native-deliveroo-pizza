109fc43b4abdb301f23cc1eb7cedd2ae39cd22d3
pankaj suthar <pankajss0070@gmail.com>
2024-03-16 12:51:02 +0530

    second commit

---
+++ src/components/Categories.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {ScrollView, Text, View} from 'react-native';
+
+const Categories = () => {
+  return (
+    <ScrollView>
+      {/* categories card */}
+      <Text>Categories</Text>
+    </ScrollView>
+  );
+};
+
+export default Categories;
--- src/screen/Home.jsx
+++ src/screen/Home.jsx
@@ -1,10 +1,24 @@
-import React, {useLayoutEffect} from 'react';
-import {Image, SafeAreaView, Text, View} from 'react-native';
+import React, {useLayoutEffect, useState} from 'react';
+import {
+  Image,
+  SafeAreaView,
+  ScrollView,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
-import {ChevronDownIcon, UserIcon} from 'react-native-heroicons/outline';
+import {
+  ChevronDownIcon,
+  UserIcon,
+  MagnifyingGlassIcon,
+  AdjustmentsHorizontalIcon,
+} from 'react-native-heroicons/outline';
+import Categories from '../components/Categories';
 
 const Home = () => {
   const navigation = useNavigation();
+  const [seacrh, setSearch] = useState('');
 
   useLayoutEffect(() => {
     navigation.setOptions({
@@ -12,21 +26,36 @@ const Home = () => {
     });
   }, []);
   return (
-    <SafeAreaView>
+    <SafeAreaView className="bg-white pt-5">
+      {/*Header*/}
       <View className="flex-row pb-3 items-center mx-4 space-x-2">
         <Image
           source={{uri: 'https://links.papareact.com/wru'}}
           className="h-7 w-7 bg-gray-300 p-4 rounded-full"
         />
-        <View>
+        <View className="flex-1">
           <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
-          <Text>
-            <ChevronDownIcon size={20} color="#00CCBB" />
+          <Text className="font-bold text-xl">
+            Current Location <ChevronDownIcon size={20} color="#00CCBB" />
           </Text>
-          <Text className="font-bold text-xl">Current Location</Text>
         </View>
         <UserIcon size={35} color="#00CCBB" />
       </View>
+
+      {/* Search */}
+      <View className="flex-row pb-2 mx-4 space-x-2 items-center">
+        <View className="flex-row space-x-2 items-center bg-gray-200 px-4  flex-1">
+          <MagnifyingGlassIcon color="gray" size={20} />
+          <TextInput placeholder="Restaurant and cuisines" />
+        </View>
+        <AdjustmentsHorizontalIcon color="gray" size={30} />
+      </View>
+      {/*body*/}
+      <ScrollView className="bg-gray-100">
+        {/*categories */}
+        <Categories />
+        {/*featured rows */}
+      </ScrollView>
     </SafeAreaView>
   );
 };
